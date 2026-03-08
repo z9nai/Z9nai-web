8d0f6a72e27ac1e16bbb6c06eed4e6a42e30fed7
Pascal Mengelt <pascal.mengelt@gmail.com>
2026-03-08 16:36:31 +0100

    state of work

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/Z9nai-web/',
     plugins: [react(), tailwindcss()],
     assetsInclude: ['**/*.pdf'],
     define: {
